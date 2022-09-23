f776a2acaf1c114dde25b395cdeb4105b8ecffa2
Dominic Hemken <dphemken@gmail.com>
2022-09-23 12:32:16 -0500

    Created Basic Event Router

---
+++ Dominisoft.NoKates.EventRouter.Common/EventPublisher.cs
@@ -0,0 +1,16 @@
+﻿using Dominisoft.Nokates.Common.Infrastructure.Helpers;
+
+namespace Dominisoft.NoKates.EventRouter.Common
+{
+    public class EventPublisher
+    {
+        private readonly string _eventRouterUrl;
+
+        public EventPublisher(string eventRouterUrl)
+        {
+            _eventRouterUrl = eventRouterUrl;
+        }
+        public bool PublishEvent<TObj>(string routingKey, TObj details) 
+            => HttpHelper.Post<bool>(_eventRouterUrl.Replace("{RoutingKey}",routingKey), details);
+    }
+}
---
+++ Dominisoft.NoKates.EventRouter/Controllers/EventController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using Dominisoft.Nokates.Common.Infrastructure.Attributes;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Dominisoft.NoKates.EventRouter.Controllers
+{
+    [Route("")]
+    [ApiController]
+    public class EventController : ControllerBase
+    {
+        [HttpGet("{routingKey}")]
+        [EndpointGroup("Events")]
+        [NoAuth]
+        public bool PublishEventByRoutingKey(string routingKey, [FromBody] string eventDetails)
+        {
+            return true;
+        }
+    }
+}
